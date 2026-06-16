05af330b998a807047f46e46f08fb52ed6964e58
Roei <roygim@gmail.com>
2026-06-16 10:16:07 +0300

    roeig

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
-builder.Services.AddSingleton<DapperContext>();
+builder.Services.AddScoped<DapperContext>();
 builder.Services.AddScoped<IUsersRepository, UsersDataMysql>();
 builder.Services.AddScoped<IUsersService, UsersService>();
 
